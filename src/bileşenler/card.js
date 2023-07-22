f90162a7a09bcf959d0f9e54447d2ba1b9e9cad3
İhsan Yazarel <ihsanyazarel@hotmail.com>
2023-07-22 15:22:20 +0300

    card finished

--- src/bileşenler/card.js
+++ src/bileşenler/card.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 const Card = (makale) => {
   // GÖREV 5
   // ---------------------
@@ -17,6 +18,26 @@ const Card = (makale) => {
   //   </div>
   // </div>
   //
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+  const headlineDiv = document.createElement("div");
+  headlineDiv.classList.add("headline");
+  headlineDiv.textContent = makale.anabaslik;
+  const authorDiv = document.createElement("div");
+  authorDiv.classList.add("author");
+  const imgContainerDiv = document.createElement("div");
+  imgContainerDiv.classList.add("img-container");
+  const img = document.createElement("img");
+  img.setAttribute("src", makale.yazarFoto);
+  const spanAuthor = document.createElement("span");
+  spanAuthor.textContent = `${makale.yazarAdi} tarafından`;
+  cardDiv.append(headlineDiv, authorDiv);
+  authorDiv.append(imgContainerDiv, spanAuthor);
+  imgContainerDiv.append(img);
+  cardDiv.addEventListener("click", () => {
+    console.log(makale.anabaslik);
+  });
+  return cardDiv;
 }
 
 const cardEkleyici = (secici) => {
@@ -28,6 +49,14 @@ const cardEkleyici = (secici) => {
   // Card bileşenini kullanarak yanıttaki her makale nesnesinden bir kart oluşturun.
   // Her cardı, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
   //
+  const cardsContainer = document.querySelector(secici);
+  axios.get("http://localhost:5001/api/makaleler").then((res) => {
+    for(const item in res.data.makaleler){
+      res.data.makaleler[item].forEach(element => {
+        cardsContainer.append(Card(element));
+      });
+    }
+  });
 }
 
 export { Card, cardEkleyici }
